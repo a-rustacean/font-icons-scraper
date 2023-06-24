16d2bc4ea304c998bcd7e3605fa883edc8504d4d
a-rustacean <dilshadplayingminecraft@outlook.com>
2023-06-24 15:48:19 +0530

    Update `install.sh`

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 cargo build --release
 rm -rf $HOME/.local/bin/scrap-icons
-ln -s $(pwd)/target/release/font-icons-scrapper $HOME/.local/bin/scrap-icons
+ln -s $(pwd)/target/release/font-icons-scraper $HOME/.local/bin/scrap-icons
 chmod u+x $HOME/.local/bin/scrap-icons
